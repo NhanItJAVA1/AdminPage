0da400250cc4a60a9849005d56a00f84300d7ea3
H82M19\Student <Student>
2025-04-02 11:29:54 +0700

    FIxing_image

--- my-app/src/AdminPage/Admin.jsx
+++ my-app/src/AdminPage/Admin.jsx
@@ -1,27 +1,56 @@
+import React, { useState, useEffect } from 'react';
 
 import './Admin.css'
 
 function Admin() {
-    return ( 
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('https://67ecae53aa794fb3222e6d6e.mockapi.io/Overview')
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    return (
         <div>
-            <h1>CSS Grid Layout</h1>
-            <p>The Grid Layout Module offers a grid-based layout system, with rows and columns.</p>
-            <p>The Grid Layout Module makes it easy to design complex and responsive web pages without using floats and positioning:</p>
             <div className="container">
-            <div className="menu">
-                <a href="#">Link 1</a>
-                <br/>
-                <a href="#">Link 2</a>
-                <br/>
-                <a href="#">Link 3</a>
-            </div>
-            <div className="content">
-            <div className="header"><h2>My Header</h2></div>
-                <h3>Lorem Ipsum</h3>
-                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ridiculus sit nisl laoreet facilisis aliquet. Potenti dignissim litora eget montes rhoncus sapien neque urna. Cursus libero sapien integer magnis ligula lobortis quam ut.</p></div>
-                <div className="footer">
-                    <h4>Footer</h4>
+                <div className="menu">
+                    <a href="#">Link 1</a>
+                    <br />
+                    <a href="#">Link 2</a>
+                    <br />
+                    <a href="#">Link 3</a>
                 </div>
+                <div className="content">
+                    <div className="header">My Header</div>
+                    <div className='overview'>
+                        {data.map(item => (
+                            <div className="card" key={item.id}>
+                                <div className="left">
+                                    <h3>{item.name}</h3>
+                                    <h2>{item.dollars}</h2>
+                                    <h5>{item.percent}%</h5>
+                                </div>
+                                <div className="right">
+                                    <img className='icon' src={item.icon} alt={item.name} />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="footer">Footer</div>
             </div>
         </div>
     );
